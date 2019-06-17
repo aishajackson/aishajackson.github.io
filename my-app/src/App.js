da074e1c4ccba1724b0e033c8ab8ddd8f9135e3b
Aisha Jackson <ajackson7148@gmail.com>
2019-06-16 20:29:27 -0700

    added navbar

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavbarHeader from './Components/Navbar/navbarHeader'
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarHeader />
+    </div>
+  );
+}
+
+export default App;
