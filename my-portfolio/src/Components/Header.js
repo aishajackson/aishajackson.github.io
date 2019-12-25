bd35af7e6832d7390413b858396e9955b383473c
Aisha Jackson <ajackson7148@gmail.com>
2019-12-24 20:06:48 -0500

    fix header portion and some styling

--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -4,10 +4,6 @@ class Header extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -25,8 +21,6 @@ class Header extends Component {
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
