a35d65697e556f3081c3a832ad5ab12642cb6655
Aisha Jackson <ajackson7148@gmail.com>
2020-02-15 19:48:10 -0800

    Updates

---
+++ precache-manifest.1a6ffc4e4a3421428077e24243a726d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213a0687ec91457a95e1664143040636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "216b9db74acef3e931c7",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a2f4026e66617dc76f3b",
+    "url": "/static/js/2.1d4e7c5a.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.1d4e7c5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "216b9db74acef3e931c7",
+    "url": "/static/js/main.5985623a.chunk.js"
+  },
+  {
+    "revision": "b6d641d7babc028ccdf2",
+    "url": "/static/js/runtime-main.712fe5d1.js"
+  }
+]);
